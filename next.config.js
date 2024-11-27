0106280fe0d8a30cdc98fdfce0f8adc346a8c2fa
Anuj12Pokharel <142072673+Anuj12Pokharel@users.noreply.github.com>
2024-11-27 14:29:03 +0545

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// next.config.js
+module.exports = {
+  output: 'export',  // Enables static export (if you're using next export)
+  images: {
+    unoptimized: true,  // Optional: if you're handling image optimization yourself
+  },
+}
